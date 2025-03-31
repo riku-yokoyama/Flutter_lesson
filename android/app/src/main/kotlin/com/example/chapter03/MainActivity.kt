6a3869c32a34b8e9217970c40cacddbc561c2c28
DairikuYoko <yokoriku@gmail.com>
2025-04-01 00:54:27 +0900

    firts commit

---
+++ android/app/src/main/kotlin/com/example/chapter03/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chapter03
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // アプリのルート定型
+  runApp(const MyApp());
+}
+
+// これも定型文<アプリのUIの基盤になるMaterialAppを返す>
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+// これも定型文<実際のUIはMyhomepageのstatefullウィジェットに書く>
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // UI構築するスキャフォールド
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+          child: Column(
+        // Centerを使用すると横は中央になるが、Columnを使用すると縦が上寄せになるので
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Centerにテキストウィジェットを配置してみる"),
+          Text(
+            "画面に長文を表示したいとき、FlutterではmaxLinesを使用することで最大行数の指定や、overflowで最大範囲を超えた際の挙動を制御できる",
+            maxLines: 2, // 最大行数を2行に指定
+            overflow: TextOverflow.ellipsis, // 未切れを…で表現する
+          ),
+          Text(
+              "もしFlutterでmaxLinesやoverflowを使用しないと、行数や見切れを制御することなく、ひたすらにすべてを表示しきるまで改行し続けて制御することなく冗長に文章が表示されます。"),
+          Text(
+            "これは青色の太文字です",
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
